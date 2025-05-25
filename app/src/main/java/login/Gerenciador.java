83ba3cbb7efb1feeb71223ef50fbf9e0c543e724
Mirella Barros <mirella.b@aluno.ifsc.edu.br>
2025-05-25 08:59:02 -0300

    Ajustes no app e na busca de login do exercicio 1.

--- app/src/main/java/login/Gerenciador.java
+++ app/src/main/java/login/Gerenciador.java
@@ -1,25 +1,58 @@
 package login;
 
-import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class Gerenciador {
-    private static Set<Usuario> usuarios = new HashSet<>();
+    private static Set<Usuario> usuarios = new LinkedHashSet<>();
 
-    public static void criarUsuario(String usuario, String senha) {
-        Usuario u = new Usuario(usuario, senha);
+    public static boolean criarUsuario(String login, String senha) {
+        // Verifica se as variáveis não estão vazias.
+        if (login.isEmpty() || senha.isEmpty()) {
+            return false;
+        }
+
+        // Cria um novo usuário e adiciona à lista
+        Usuario u = new Usuario(login.toLowerCase(), senha);
+
+        // Verifica se o login já existe
+        if (usuarios.contains(u)) {
+            return false; // login já existe
+        }
+
         usuarios.add(u);
+        return true;
     }
 
-    public static void removerUsuario {
-
+    public static boolean removerUsuario(String login) {
+        if (!login.isEmpty()) {
+            if (usuarios.removeIf(u -> u.getLogin().equals(login.toLowerCase()))) {
+                return true;
+            }
+        }
+        return false;
     }
 
-    public static boolean login {
-
+    public static boolean login(String login, String senha) {
+        if (!login.isEmpty() || !senha.isEmpty()) {
+            for (Usuario u : usuarios) {
+                if (u.getLogin().equals(login.toLowerCase())) {
+                    if (u.getSenha().equals(senha)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 
-    public static void listarUsuarios {
-
+    public static String[] listarUsuarios() {
+        String[] lista = new String[usuarios.size()];
+        int i = 0;
+        for (Usuario u : usuarios) {
+            lista[i] = u.getLogin();
+            i++;
+        }
+        return lista;
     }
 }
--- app/src/main/java/login/Usuario.java
+++ app/src/main/java/login/Usuario.java
@@ -1,19 +1,41 @@
 package login;
+import java.util.Objects;
 
 public class Usuario {
-    private String usuario;
+    private String login;
     private String senha;
 
-    public Usuario(String usuario, String senha) {
-        this.usuario = usuario;
+    public Usuario(String login, String senha) {
+        this.login = login;
         this.senha = senha;
     }
 
-    public String getUsuario() {
-        return usuario;
+    public String getLogin() {
+        return login;
     }
 
     public String getSenha() {
         return senha;
+    }
+
+    // Sobrescreve o equals.
+    // Compara o login (e não todos os atributos ou o endereço de memória).
+    @Override
+    public boolean equals(Object obj) {
+        // Verifica se é o mesmo objeto (mesmo endereço de memória)
+        if (this == obj) return true;
+
+        // Verifica se é um objeto do tipo Usuario
+        if (!(obj instanceof Usuario)) return false;
+
+        // Verifica se o login já existe
+        Usuario u = (Usuario) obj;
+        return this.login.equals(u.login);
+    }
+
+    // Sobrescreve o hashCode
+    @Override
+    public int hashCode() {
+        return Objects.hash(login);
     }
 }
